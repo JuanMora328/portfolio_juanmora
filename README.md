152746aaf99dff12ce84c17f1ba313def4f0209a
Juan Pablo Mora Sarria <69485570+JuanMora328@users.noreply.github.com>
2023-06-02 23:25:57 -0500

    Portfolio

---
+++ README.md
@@ -0,0 +1,52 @@
+Dear John Doe,
+
+I wanted to take a moment to express my deepest gratitude for the template you provided. Your thoughtfulness and generosity have been truly remarkable.
+
+The template you shared has been an invaluable resource, saving me precious time and effort. It has not only streamlined my work but also helped me achieve better results. Your expertise and attention to detail are evident in every aspect of the template, and I am sincerely grateful for your dedication.
+
+Your willingness to share your knowledge and skills is truly commendable. You have made a significant impact on my work, and I cannot thank you enough for your contribution. Your template has not only enriched my professional endeavors but also inspired me to pay it forward and assist others whenever I can.
+
+Once again, thank you from the bottom of my heart for your exceptional template. Your generosity and selflessness are qualities that inspire and motivate me to strive for excellence. I am truly fortunate to have crossed paths with someone as remarkable as you.
+
+With heartfelt appreciation,
+
+Juan Mora.
+
+### Description
+
+A simple portfolio template for developer/designers built with React. 
+
+### [live preview](https://ubaimutl.github.io/react-portfolio/)
+
+[![react portfoiio](src/assets/images/react%20portfolio%20gif.gif)](https://ubaimutl.github.io/react-portfolio/)
+
+### Features
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
+### Setup
+
+Get the code
+
+<pre>git clone https://github.com/ubaimutl/react-portfolio.git</pre>
+ 
+Install required dependencies
+
+<pre>yarn install</pre>
+
+
+Start the server
+
+<pre>yarn start</pre>
+
+### More
+
+Modify pages content in  `src/content_option.js`.
+
+### Thanks
+
+If you like this portfolio template don't forget give it a ⭐ 
---
+++ src/content_option.js
@@ -0,0 +1,137 @@
+const logotext = "Juescami";
+const meta = {
+    title: "Juan Mora",
+};
+
+const introdata = {
+    title: "Hi i'm Juan Mora",
+    animated: {
+        first: "Skillful Coder",
+        second: "Problem solver",
+        third: "Tech Innovator",
+    },
+    description: "Hey, what's up? Welcome to my portfolio as a backend developer, where I'm excited to showcase my projects. Here, you'll find a collection of my work that highlights my skills and expertise in backend development.",
+    your_img_url: "https://www.in2code.de/fileadmin/_processed_/0/b/csm_code_javascript_49d002a67e.webp",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Computer Engineer with a year of dedicated backend development experience. Proficient in Java, Spring Boot, Node.js, Express.js, and JavaScript, I am passionate about crafting efficient and scalable solutions. Known for my strong sense of responsibility, meticulous problem analysis, and collaborative spirit, I thrive in team environments. Let's join forces to build remarkable software solutions that make an impact!",
+};
+const worktimeline = [{
+        jobtitle: "Developer APX",
+        where: "INDRA S.A.S",
+        date: "August 2022 - April 2023",
+    },
+    {
+        jobtitle: "Operations Engineer",
+        where: "Scouts Centinelas",
+        date: "July - November 2022",
+    },
+    {
+        jobtitle: "Backend Developer",
+        where: "Ceocol",
+        date: "January - June 2022",
+    },
+];
+
+const skills = [{
+        name: "JAVA",
+        value: 80,
+    },
+    {
+        name: "JAVA SpringBoot",
+        value: 75,
+    },
+    {
+        name: "Node JS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React JS",
+        value: 60,
+    },
+    {
+        name: "Flutter",
+        value: 70,
+    },
+    {
+        name: "Relational databases",
+        value: 80,
+    },
+    {
+        name: "NoSQL databases",
+        value: 65,
+    },
+];
+
+const services = [{
+        title: "Professional Web Development",
+        description: "Need a stunning website? Look no further! As a skilled web developer, I specialize in creating dynamic and engaging web experiences. Leveraging the power of Node.js, Express.js, JavaScript, and PHP, I deliver top-notch websites that captivate users. Let's bring your vision to life and make your online presence shine!",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Do you want a mobile app? Look no further! I specialize in custom cross-platform mobile app development, using Flutter to bring your designs to life. With meticulous attention to detail, I'll transform your vision into a polished and seamless app experience for both iOS and Android. Let's collaborate and create a stunning mobile app that captivates your audience! ",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://www.team-parallax.com/static/8b2d49f70e921fd2920602d0cd51e31a/0a8d8/illu-mern.png",
+        description: "The web application helps the accounting management of payments to operators and collection from insurers, since it allows keeping the accounts of a organized and systematized way.",
+        link: "https://github.com/JuanMora328/BACKEND_CEOCOL",
+    },
+    {
+        img: "https://static.vecteezy.com/system/resources/thumbnails/007/476/207/small_2x/icon-api-suitable-for-programming-symbol-long-shadow-style-simple-design-editable-design-template-simple-symbol-illustration-vector.jpg",
+        description: "Basic API in JavaScript using Node JS, Express JS and Mongodb.",
+        link: "https://github.com/JuanMora328/APIREST_MEN",
+    },
+    {
+        img: "https://pbs.twimg.com/profile_images/980963748/Vemos_Cine_400x400.png",
+        description: "Application using flutter and API that show the films, description and the cast.",
+        link: "https://github.com/JuanMora328/APPPeliculas_FLUTTER",
+    },
+    {
+        img: "https://www.formbot.com/images/how-to-scan-a-qr-code.png",
+        description: "This is a qr reader app, the qrs allowed are: Links http and localization geo. Also this app uses maps api.",
+        link: "https://github.com/JuanMora328/qr_reader_FlutterAPP",
+    },
+    {
+        img: "https://pbs.twimg.com/profile_images/1187814172307800064/MhnwJbxw_400x400.jpg",
+        description: "This project was created to discover the differents widgets / components from FLUTTER.",
+        link: "https://github.com/JuanMora328/FLUTTER_COMPONENTS_WIDGETS",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "juanpablomora777@gmail.com",
+    YOUR_FONE: "(+57)301 636 5097",
+    description: "Let's build something incredible together! Reach out to me and let's discuss how we can bring your ideas to life. Don't hesitate to contact me today!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_kir4hwz",
+    YOUR_TEMPLATE_ID: "template_q4x3ml8",
+    YOUR_USER_ID: "jAY91ATaZ0thknOE9",
+};
+
+const socialprofils = {
+    github: "https://github.com/JuanMora328",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/juanpablomorasarria/",
+    twitter: "https://twitter.com/DevJuescami",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
