ed123af0abdec5f91f423b7cf2587a9fee049c6b
Juan Pablo Mora Sarria <69485570+JuanMora328@users.noreply.github.com>
2023-08-19 08:58:00 -0500

    Update new version
    Add my new job, and update my cv to download the last version

--- src/content_option.js
+++ src/content_option.js
@@ -19,101 +19,129 @@ const dataabout = {
     aboutme: "Computer Engineer with a year of dedicated backend development experience. Proficient in Java, Spring Boot, Node.js, Express.js, and JavaScript, I am passionate about crafting efficient and scalable solutions. Known for my strong sense of responsibility, meticulous problem analysis, and collaborative spirit, I thrive in team environments. Let's join forces to build remarkable software solutions that make an impact!",
 };
 const worktimeline = [{
-        jobtitle: "Developer APX",
-        where: "INDRA S.A.S",
-        date: "August 2022 - April 2023",
-    },
-    {
-        jobtitle: "Operations Engineer",
-        where: "Scouts Centinelas",
-        date: "July - November 2022",
-    },
-    {
-        jobtitle: "Backend Developer",
-        where: "Ceocol",
-        date: "January - June 2022",
-    },
+    jobtitle: "Junior Developer",
+    where: "Visitrack S.A.S",
+    date: "July 2023 - Currently",
+},
+{
+    jobtitle: "Developer APX",
+    where: "INDRA S.A.S",
+    date: "August 2022 - April 2023",
+},
+{
+    jobtitle: "Operations Engineer",
+    where: "Scouts Centinelas",
+    date: "July - November 2022",
+},
+{
+    jobtitle: "Backend Developer",
+    where: "AMCOR",
+    date: "July - December 2022",
+},
+{
+    jobtitle: "Developer",
+    where: "Fitbox",
+    date: "January - June 2022",
+},
+{
+    jobtitle: "Backend Developer",
+    where: "Ceocol",
+    date: "January - June 2022",
+}
 ];
 
 const skills = [{
-        name: "JAVA",
-        value: 80,
-    },
-    {
-        name: "JAVA SpringBoot",
-        value: 75,
-    },
-    {
-        name: "Node JS",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React JS",
-        value: 60,
-    },
-    {
-        name: "Flutter",
-        value: 70,
-    },
-    {
-        name: "Relational databases",
-        value: 80,
-    },
-    {
-        name: "NoSQL databases",
-        value: 65,
-    },
+    name: "JAVA",
+    value: 80,
+},
+
+{
+    name: "JAVA SpringBoot",
+    value: 75,
+},
+
+{
+    name: "Node JS",
+    value: 85,
+},
+
+{
+    name: "Javascript",
+    value: 80,
+},
+
+{
+    name: "React JS",
+    value: 60,
+},
+
+{
+    name: "Flutter",
+    value: 70,
+},
+
+{
+    name: "Relational databases",
+    value: 80,
+},
+
+{
+    name: "NoSQL databases",
+    value: 65,
+},
+
+{
+    name: "Server Management",
+    value: "70"
+}
 ];
 
 const services = [{
-        title: "Professional Web Development",
-        description: "Need a stunning website? Look no further! As a skilled web developer, I specialize in creating dynamic and engaging web experiences. Leveraging the power of Node.js, Express.js, JavaScript, and PHP, I deliver top-notch websites that captivate users. Let's bring your vision to life and make your online presence shine!",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Do you want a mobile app? Look no further! I specialize in custom cross-platform mobile app development, using Flutter to bring your designs to life. With meticulous attention to detail, I'll transform your vision into a polished and seamless app experience for both iOS and Android. Let's collaborate and create a stunning mobile app that captivates your audience! ",
-    },
+    title: "Professional Web Development",
+    description: "Need a stunning website? Look no further! As a skilled web developer, I specialize in creating dynamic and engaging web experiences. Leveraging the power of Node.js, Express.js, JavaScript, and PHP, I deliver top-notch websites that captivate users. Let's bring your vision to life and make your online presence shine!",
+},
+
+{
+    title: "Mobile Apps",
+    description: "Do you want a mobile app? Look no further! I specialize in custom cross-platform mobile app development, using Flutter to bring your designs to life. With meticulous attention to detail, I'll transform your vision into a polished and seamless app experience for both iOS and Android. Let's collaborate and create a stunning mobile app that captivates your audience! ",
+}
 ];
 
 const dataportfolio = [{
-        img: "https://www.team-parallax.com/static/8b2d49f70e921fd2920602d0cd51e31a/0a8d8/illu-mern.png",
-        description: "The web application helps the accounting management of payments to operators and collection from insurers, since it allows keeping the accounts of a organized and systematized way.",
-        link: "https://github.com/JuanMora328/BACKEND_CEOCOL",
-    },
-    {
-        img: "https://static.vecteezy.com/system/resources/thumbnails/007/476/207/small_2x/icon-api-suitable-for-programming-symbol-long-shadow-style-simple-design-editable-design-template-simple-symbol-illustration-vector.jpg",
-        description: "Basic API in JavaScript using Node JS, Express JS and Mongodb.",
-        link: "https://github.com/JuanMora328/APIREST_MEN",
-    },
-    {
-        img: "https://pbs.twimg.com/profile_images/980963748/Vemos_Cine_400x400.png",
-        description: "Application using flutter and API that show the films, description and the cast.",
-        link: "https://github.com/JuanMora328/APPPeliculas_FLUTTER",
-    },
-    {
-        img: "https://www.formbot.com/images/how-to-scan-a-qr-code.png",
-        description: "This is a qr reader app, the qrs allowed are: Links http and localization geo. Also this app uses maps api.",
-        link: "https://github.com/JuanMora328/qr_reader_FlutterAPP",
-    },
-    {
-        img: "https://pbs.twimg.com/profile_images/1187814172307800064/MhnwJbxw_400x400.jpg",
-        description: "This project was created to discover the differents widgets / components from FLUTTER.",
-        link: "https://github.com/JuanMora328/FLUTTER_COMPONENTS_WIDGETS",
-    },
-    {
-        img: "https://netlibsecurity.com/wp-content/uploads/2016/01/sqlserver-logo.png",
-        description: "The Node.js API, powered by Express.js, is a basic RESTful API that allows clients to interact with a SQL Server database.",
-        link: "https://github.com/JuanMora328/APIREST_SQLSERVER",
-    },
-    {
-        img: "https://pbs.twimg.com/profile_images/1452637606559326217/GFz_P-5e_400x400.png",
-        description: "The Node.js API, powered by Express.js, is a basic RESTful API designed specifically for practicing relations in MongoDB.",
-        link: "https://github.com/JuanMora328/API_MONGO-NODEJS",
-    },
+    img: "https://www.team-parallax.com/static/8b2d49f70e921fd2920602d0cd51e31a/0a8d8/illu-mern.png",
+    description: "The web application helps the accounting management of payments to operators and collection from insurers, since it allows keeping the accounts of a organized and systematized way.",
+    link: "https://github.com/JuanMora328/BACKEND_CEOCOL",
+},
+{
+    img: "https://static.vecteezy.com/system/resources/thumbnails/007/476/207/small_2x/icon-api-suitable-for-programming-symbol-long-shadow-style-simple-design-editable-design-template-simple-symbol-illustration-vector.jpg",
+    description: "Basic API in JavaScript using Node JS, Express JS and Mongodb.",
+    link: "https://github.com/JuanMora328/APIREST_MEN",
+},
+{
+    img: "https://pbs.twimg.com/profile_images/980963748/Vemos_Cine_400x400.png",
+    description: "Application using flutter and API that show the films, description and the cast.",
+    link: "https://github.com/JuanMora328/APPPeliculas_FLUTTER",
+},
+{
+    img: "https://www.formbot.com/images/how-to-scan-a-qr-code.png",
+    description: "This is a qr reader app, the qrs allowed are: Links http and localization geo. Also this app uses maps api.",
+    link: "https://github.com/JuanMora328/qr_reader_FlutterAPP",
+},
+{
+    img: "https://pbs.twimg.com/profile_images/1187814172307800064/MhnwJbxw_400x400.jpg",
+    description: "This project was created to discover the differents widgets / components from FLUTTER.",
+    link: "https://github.com/JuanMora328/FLUTTER_COMPONENTS_WIDGETS",
+},
+{
+    img: "https://netlibsecurity.com/wp-content/uploads/2016/01/sqlserver-logo.png",
+    description: "The Node.js API, powered by Express.js, is a basic RESTful API that allows clients to interact with a SQL Server database.",
+    link: "https://github.com/JuanMora328/APIREST_SQLSERVER",
+},
+{
+    img: "https://pbs.twimg.com/profile_images/1452637606559326217/GFz_P-5e_400x400.png",
+    description: "The Node.js API, powered by Express.js, is a basic RESTful API designed specifically for practicing relations in MongoDB.",
+    link: "https://github.com/JuanMora328/API_MONGO-NODEJS",
+},
 ];
 
 const contactConfig = {
